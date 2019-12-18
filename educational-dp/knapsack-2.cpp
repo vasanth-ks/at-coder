6bbb92fa484dfb096f959d1bbc9b01dfcc7ffa8c
vasanth-ks <vasanth.sethuraman@gmail.com>
2019-12-17 16:36:00 -0800

    Modified knapsack-1 implementation and added solution for knapsack-2

--- educational-dp/knapsack-2.cpp
+++ educational-dp/knapsack-2.cpp
@@ -1,10 +1,10 @@
-/*
-https://atcoder.jp/contests/dp/tasks/dp_d
-*/
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
+
+const long long INF = 1e18L + 7;
 
 struct Knapsack {
     int weight, value;
@@ -16,19 +16,27 @@ long long getMaxValue (vector <Knapsack> &knapsack, int capacity) {
     if (knapsack.empty()) {
         return 0;
     }
-    int n = knapsack.size();
-    vector <long long> dp(capacity + 1);
+    int n = knapsack.size(), totalValue = 0;
+
+    for (auto &k : knapsack) {
+        totalValue += k.value;
+    }
+    vector <long long> dp(totalValue + 1, INF);
+    dp[0] = 0;
     
     for (int i = 0; i < n; ++i) {
         int currWeight = knapsack[i].weight;
         int currValue = knapsack[i].value;
-        for (int c = capacity; c >= currWeight; --c) {
-            dp[c] = max(dp[c], dp[c - currWeight] + (long long)currValue);
+        for (int v = totalValue; v >= currValue; --v) {
+            dp[v] = min(dp[v], dp[v - currValue] + currWeight);
         }
     }
     long long result = 0;
-    for (long long v : dp) {
-        result = max(result, v);
+
+    for (long long v = 0; v <= totalValue; ++v) {
+        if (dp[v] <= capacity) {
+            result = max(result, v);
+        }
     }
     return result;
 }
